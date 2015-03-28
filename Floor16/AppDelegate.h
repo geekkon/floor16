20485263504fe86947fe755054d779ddd719d096
Dim <dim@MacBook-Pro-Dim.local>
2015-03-28 12:51:07 +0400

    Initial Commit

---
+++ Floor16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Floor16
+//
+//  Created by Dim on 28.03.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Floor16/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Floor16
+//
+//  Created by Dim on 28.03.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
