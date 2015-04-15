664be1bb317ddd79857656bdc0f7bf00b9923254
Dim <dim@MacBook-Pro-Dim.local>
2015-04-15 23:39:26 +0400

    Process

---
+++ Floor16/DBItemDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DBItemDetailTableViewController.h
+//  Floor16
+//
+//  Created by Dim on 15.04.15.
+//  Copyright (c) 2015 Dmitriy Baklanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBItemDetailTableViewController : UITableViewController
+
+@end
